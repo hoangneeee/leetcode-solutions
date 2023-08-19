75c7c54a509fe0c03f0708b3c64cd09cff8d2e05
Hoàng Nè <levuthanhtung11@gmail.com>
2023-08-19 14:21:43 +0700

    refactor(stack): daily temperatures

---
+++ Stack/DailyTemperatures.ts
@@ -0,0 +1,17 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+  if (temperatures.length === 1) return [0];
+  
+  const stack = [];
+  const result = new Array(temperatures.length).fill(0);
+  
+  for (let i = 0; i < temperatures.length; i++) {
+    while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+      const idx = stack.pop();
+      result[idx] = i - idx;
+    }
+    stack.push(i);
+  }
+  return result;
+};
+
+console.log(dailyTemperatures([73,74,75,71,69,72,76,73]));
