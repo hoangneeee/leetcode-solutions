0231b1dab198e5ac09fd6f74b0771683b462e8de
Hoàng Nè <levuthanhtung11@gmail.com>
2023-09-15 09:08:16 +0700

    refactor(two-pointers): two sum 2

---
+++ TwoPointers/TwoSumII-InputArrayIsSorted.ts
@@ -0,0 +1,14 @@
+function twoSum(numbers: number[], target: number): number[] {
+  const hash = {};
+
+  for (let i = 0; i < numbers.length; i++) {
+    const n = numbers[i];
+    if (hash.hasOwnProperty(target - n)) {
+      return [hash[target - n] + 1, i + 1];
+    }
+    hash[n] = i;
+  }
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
